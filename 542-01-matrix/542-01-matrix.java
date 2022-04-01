d94b73f3fbe270eca60c2d7e6ee3037409dd4dae
Shobham Rajak <shobham.rajak@gmail.com>
2022-04-02 00:02:28 +0530

    Time: 28 ms (25.49%), Space: 73.4 MB (30.12%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -2,9 +2,7 @@ class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int n=mat.length;
         int m=mat[0].length;
-        
         LinkedList<Integer> que=new LinkedList<>();
-        
         int[][] ans=new int[n][m];
         
         for(int[] e:ans){
@@ -21,21 +19,17 @@ class Solution {
         }
         
         int[][] dirs={{0,1},{1,0},{0,-1},{-1,0}};
-        
         int level=0;
+        
         while(que.size()>0){
             int size=que.size();
-            
             while(size-->0){
                 int idx=que.removeFirst();
-                
                 int i=idx/m;
                 int j=idx%m;
-                
                 for(int[] dir:dirs){
                     int x=i+dir[0];
                     int y=j+dir[1];
-                    
                     if(x>=0 && y>=0 && x<n && y<m && ans[x][y]==-1){
                         que.addLast(x*m+y);
                         ans[x][y]=level+1;
@@ -44,7 +38,6 @@ class Solution {
             }
             level++;
         }
-        
         return ans;
     }
 }
