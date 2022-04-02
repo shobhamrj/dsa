2dc06659ad08ea17da7d5df3385a6775894211b0
Shobham Rajak <shobham.rajak@gmail.com>
2022-04-02 22:20:23 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (37.94%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,7 +1,16 @@
 class Solution {
     public int fib(int N) {
-        if(N <= 1) return N;
-        return fib(N - 1) + fib(N - 2);
+        if(N == 0)
+            return 0;
+        int[] lastTwo = {1, 1};
+        int counter = 3;
+        while(counter <= N) {
+            int nextFib = lastTwo[0] + lastTwo[1];
+            lastTwo[0] = lastTwo[1];
+            lastTwo[1] = nextFib;
+            counter +=1;
+        }
+        return lastTwo[1];
         
     }
 }
