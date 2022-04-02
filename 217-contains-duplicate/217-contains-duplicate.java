e7000661e6f6fec6a05fe343901abaf6ed6ad3ad
Shobham Rajak <shobham.rajak@gmail.com>
2022-04-02 16:01:19 +0530

    Time: 5 ms (97.48%), Space: 53.1 MB (96.75%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+ 
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>(nums.length);
+        for(int i :nums) {
+            if(set.contains(i))
+                return true;
+            set.add(i);
+        }
+        return false;
+    }
+    
+}
