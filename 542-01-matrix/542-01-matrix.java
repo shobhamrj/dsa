d132b771d521395f7db483d0da958df66fe4adec
Shobham Rajak <shobham.rajak@gmail.com>
2022-04-05 00:41:22 +0530

    Time: 20 ms (53.64%), Space: 72.4 MB (54.26%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -2,42 +2,43 @@ class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
-        LinkedList<Integer> que = new LinkedList<>();
+        LinkedList<Integer> q = new LinkedList<>();
         int[][] ans = new int[n][m];
-        
+
         for(int[] e : ans){
             Arrays.fill(e, -1); // -1 -> unvisited
         }
         
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
                 if(mat[i][j] == 0){
-                    que.addLast(i * m + j);
+                    q.addLast(i * m + j);
                     ans[i][j] = 0;
                 }
             }
         }
         
-        int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         int level = 0;
         
-        while(que.size() > 0){
-            int size = que.size();
-            while(size-- > 0){
-                int idx = que.removeFirst();
+        while(q.size() > 0){
+            int s = q.size();
+            while(s-- > 0){
+                int idx = q.removeFirst();
                 int i = idx / m;
                 int j = idx % m;
                 for(int[] dir : dirs){
                     int x = i + dir[0];
                     int y = j + dir[1];
                     if(x >= 0 && y >= 0 && x < n && y < m && ans[x][y] == -1){
-                        que.addLast(x * m + y);
+                        q.addLast(x * m + y);
                         ans[x][y] = level + 1;
                     }
                 }
             }
-            level++;
+            level ++;
         }
         return ans;
+        
     }
 }
