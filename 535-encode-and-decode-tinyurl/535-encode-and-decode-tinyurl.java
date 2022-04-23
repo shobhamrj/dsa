c0fe0158609117995b485ced70b2dcfee94182a0
Shobham Rajak <shobham.rajak@gmail.com>
2022-04-23 14:11:57 +0530

    refactor

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.java
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.java
@@ -1,17 +1,17 @@
 public class Codec {
+  
+  static final Map<String, String> map = new HashMap<>();
+  static final String BASE_URL = "http://tinyurl.com/";
 
-	static final Map<String, String> map = new HashMap<>();
-    static final String BASE_URL = "http://tinyurl.com/";
-
-    public String encode(String longUrl){
+  public String encode(String longUrl){
 		String shortUrl = BASE_URL + longUrl.hashCode();
 		map.put(shortUrl, longUrl);
 		return shortUrl;
-    }
+  }
 
-    public String decode(String shortUrl){
+  public String decode(String shortUrl){
 		return map.get(shortUrl);
-    }
+  }
 }
 
 // Your Codec object will be instantiated and called as such:
